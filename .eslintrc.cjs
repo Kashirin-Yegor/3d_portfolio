45f3d3b97601ed2c18b8e0024392ab708c506a3f
Kashirin Yegor <egor4815162342@yandex.ru>
2024-02-18 14:43:19 +0300

    finish project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs','src'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
@@ -16,5 +16,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "react/no-unknown": 'off',
+    "react/no-unknown-property": 'off',
+    "react/prop-types": 'off',
   },
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export {Home} from './Home.jsx'
+export {Contact} from './Contact.jsx'
+export {About} from './About.jsx'
+export {Projects} from './Projects.jsx'
